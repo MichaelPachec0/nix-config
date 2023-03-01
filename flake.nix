9828341da4615bcefabc9a0c02070914349c2c65
Michael Pacheco <git@michaelpacheco.org>
2023-03-01 12:10:09 -0700

    init: Initial configuration.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = " Nix infrastructure config";
+
+  inputs = {
+    nixpkgs = { url = "nixpkgs/nixos-22.11"; };
+    nixpkgs-unstable = { url = "nixos/nixos-unstable"; };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hardware = { url = "github:nixos/nixos-hardware/master"; };
+  };
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+    let
+      inherit (self) ouptuts;
+      unstable = import nixpkgs-unstable {
+        config.allowUnfree = true;
+        system = "x86_64-linux";
+        overlays = [ ];
+      };
+      overlayUnstable = final: prev: { inherit unstable; };
+      baseModules = [
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlayUnstable ]; })
+      ];
+      nixosModules = baseModules ++ [
+        home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+        }
+      ];
+    in {
+      nixosConfiguration = with nixpkgs.lib; {
+        nyx = nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs outputs; };
+          modules = nixosModules ++ [ ./nixos/configuration.nix ];
+        };
+      };
+    };
+
+}
