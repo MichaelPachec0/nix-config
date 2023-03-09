01d2d8e5e998c5c7c7b979b8d8a81024f1b29db6
Michael Pacheco <git@michaelpacheco.org>
2023-03-08 20:00:35 -0700

    feat: boot: enable memtest and group boot options.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,8 +76,13 @@
   networking.firewall.enable = false;
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      memtest86.enable = true;
+    };
+    efi.canTouchEfiVariables = false;
+  };
 
   time.timeZone = "America/Los_Angeles";
 
