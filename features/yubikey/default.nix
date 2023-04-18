4128ba70582fca311efab135708d030b4a261b5b
Michael Pacheco <git@michaelpacheco.org>
2023-04-18 16:26:59 -0700

    FIX: build.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/yubikey/default.nix
+++ features/yubikey/default.nix
@@ -1,8 +1,8 @@
 { config, lib, ... }:
-let cfg = config.nixos;
+let cfg = config;
 in {
+  imports = [ ./yubikey.nix ];
   options = {
     yubikey = { enable = lib.mkEnableOption "Enables auth support in nixOS"; };
   };
-  config = lib.mkIf cfg.yubikey.enable { imports = [ ./yubikey.nix ]; };
 }
