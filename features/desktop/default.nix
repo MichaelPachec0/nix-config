d6f3357701a32504d3bb0a2f0db2c4ef3645ea32
Michael Pacheco <git@michaelpacheco.org>
2023-04-19 19:13:02 -0700

    FEAT: desktop: move wayland stuff into its own folder. Desktop now has common apps that are usefull in either a wayland or x11 scenario.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/desktop/default.nix
+++ features/desktop/default.nix
@@ -1,93 +1,55 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, ... }:
 let cfg = config.desktop;
 in {
+  imports = [ ./wayland ];
   options = {
     desktop = {
-      laptop = lib.mkEnableOption "Graphical configuration for laptop";
-      desktop = lib.mkEnableOption "Graphical configuration for desktop";
+      common = {
+        enable = lib.mkEnableOption
+          "Enables common desktop apps to install in a graphical environment. Apps work both in Wayland/x11.";
+      };
     };
   };
-  imports = [ ];
-  config = lib.mkIf cfg.laptop || cfg.desktop {
-    nix = {
-      binaryCachePublicKeys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
-      binaryCaches = [ "https://nixpkgs-wayland.cachix.org" ];
-    };
-    nixpkgs.overlays = [ inputs.nixpkgs-wayland.overlay ];
+  config = lib.mkIf cfg.enable {
+    assertions = [{
+      # will add all the backends here (wayland/x11)
+      assertion = cfg.wayland.laptop || cfg.wayland.desktop;
+      message = "Apps here need a X or Wayland backend!";
+    }];
     environment.systemPackages = with pkgs; [
-      # mpv as wallpaper 
-      # info: https://github.com/GhostNaN/mpvpaper
-      mpvpaper
-      cliphist
-      eww-wayland
-      # auto brightness adjustement
-      # more info: https://github.com/maximbaz/wluma
-      wluna
-      # drm devices utility
-      drm_info
-      # rdp client for wayland
-      freerdp3
-      # render glsl shaders as wallpaper
-      glpaper
-      # grab from wayland compositor
-      grim
-      # image viewer
-      imv
-      # auto configure displays
-      kanshi
-      # notification daemon
-      mako
-      # might change to this notification deamon
-      # info: https://gitlab.com/snakedye/salut
-      # salut
-      # screenshot ulity (might switch to this)
-      # info: https://git.sr.ht/~whynothugo/shotman
-      # shotman
-      # region selection for wayland
-      slurp
-      # wallpaper tool more info look at swaybg(1)
-      swaybg
-      # idle deamon info: https://github.com/swaywm/swayidle/blob/master/swayidle.1.scd
-      swayidle
-      # vanilla locker
-      # swaylock
-      # info: https://github.com/mortie/swaylock-effects
-      swaylock-effects
-      # fancy wallpaper manager
-      # info: https://github.com/Horus645/swww
-      swww
-      # multiuse prompter, supports pinentry (accessed as pinentry-wayprompt),
-      #   himitsu (himitsu-wayprompt) and generic prompt (wayprompt-cli)
-      wayprompt
-      # event viewer
-      wev
-      # screen recording
-      wf-recorder
-      # clipboard
-      wl-clipboard
-      # contrast/brightness/gamma adjuster (using a bar like waybar)
-      wl-gammarelay-rs
-      # monitor config creator
-      wlay
-      # app launcher
-      wldash
-      # logout menu
-      wlogout
-      # day/night gamma adjuster
-      wlsunset
-      # vnc client
-      wlvncc
-      # main launcher
-      wofi
-      # shows keys pressed
-      wshowkeys
-      # wayland alternative for xdotool
-      wtype
+      # pdf readers, will eventually choose between one or the other
+      evince
+      okular
 
+      # mail clients
+      neomutt
+
+      # video players
+      mpv
+      vlc
+
+      # telegram
+      ## cli
+      tg
+      ## telegram desktop
+      kotogram
+      ## ncurses telegram + whatapp
+      nchat
+      ## TODO: find out how this works
+      # matterbridge
+
+      # slack
+      slack-term
+      slack
+
+      # discord
+      ## oss discord client
+      ripcord
+      ## discord in latest electron, also privacy oriented
+      webcord
+      ## discord in terminal
+      discordo
     ];
-
   };
-
 }
+
