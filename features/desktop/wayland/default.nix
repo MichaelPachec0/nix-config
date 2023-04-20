e181d86a682df77acb3d50cbb9e3e5ac08004870
Michael Pacheco <git@michaelpacheco.org>
2023-04-19 19:24:22 -0700

    FEAT: desktop/wayland: FIX: wrong pub key.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/desktop/wayland/default.nix
+++ features/desktop/wayland/default.nix
@@ -13,7 +13,7 @@ in {
   config = lib.mkIf cfg.laptop || cfg.desktop {
     nix = {
       binaryCachePublicKeys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       ];
       binaryCaches = [ "https://nixpkgs-wayland.cachix.org" ];
     };
