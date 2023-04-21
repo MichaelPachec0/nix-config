c0657fa0de7140701dd8c28fe9cf292e785e28f5
Michael Pacheco <git@michaelpacheco.org>
2023-04-20 22:54:22 -0700

    FEAT: kitty: init config.
    * Need an overlay to track both kitty and kitty-themes as unstable.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/hm/kitty/default.nix
+++ features/hm/kitty/default.nix
@@ -1,18 +1,16 @@
-{ pkgs, ... }:
-{
-  imports = [];
-  options = {};
+{ pkgs, ... }: {
+  imports = [ ];
+  options = { };
   config = {
     nixpkgs = {
-      overlay = [
-        (final: prev: { kitty-themes = pkgs.unstable.kitty-themes; } )
-      ];
+      overlay =
+        [ (final: prev: { kitty-themes = pkgs.unstable.kitty-themes; }) ];
     };
     programs = {
       kitty = {
         enable = true;
         package = pkgs.unstable.kitty;
-        theme  = "Gruvbox Material Dark Hard";
+        theme = "Gruvbox Material Dark Hard";
       };
     };
   };
