8fb7bc790ebc2112d92b1c1e480b8c722c5452a6
Michael Pacheco <git@michaelpacheco.org>
2023-04-20 22:41:33 -0700

    FEAT: zsh: init config.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

---
+++ features/hm/zsh/plugins.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  imports = [ ];
+  options = { };
+  config = {
+    programs.zsh = {
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = ./p10k-config;
+          file = "p10k.zsh";
+        }
+      ];
+    };
+  };
+}
