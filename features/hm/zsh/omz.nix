f9e3941c125ba8a7c5203789eab7821343497fbd
Michael Pacheco <git@michaelpacheco.org>
2023-04-20 22:41:33 -0700

    FEAT: zsh: init config.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

---
+++ features/hm/zsh/omz.nix
@@ -0,0 +1,52 @@
+{ ... }: {
+  imports = [ ];
+  options = { };
+  config = {
+    programs.zsh = {
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "thefuck"
+          "aliases"
+          "battery"
+          "colored-man-pages"
+          "colorize"
+          "cp"
+          "docker"
+          "docker-compose"
+          "docker-machine"
+          "gitfast"
+          "dotenv"
+          "encode64"
+          "extract"
+          "fancy-ctrl-z"
+          "frontend-search"
+          "fzf"
+          "git-escape-magic"
+          "git-flow"
+          "gh"
+          "golang"
+
+          "kubectl"
+          "kubectx"
+          "kube-ps1"
+          "last-working-dir"
+          "nmap"
+          "node"
+          "npm"
+          "postgres"
+          "python"
+          "ripgrep"
+          "rust"
+          "safe-paste"
+          "ssh-agent"
+          "sudo"
+          "systemd"
+          #"systemadmin"
+
+        ];
+      };
+    };
+  };
+}
+
