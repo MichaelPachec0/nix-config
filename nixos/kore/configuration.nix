473f312545df7ab52f6b181958baa98c561be1ed
Michael Pacheco <git@michaelpacheco.org>
2023-04-22 17:41:04 -0700

    NIXOS: kore: missing attributes in param set, fix.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- nixos/kore/configuration.nix
+++ nixos/kore/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 let fsOptions = [ "compress=zstd" "noatime" ];
 in {
   imports = [ # Include the results of the hardware scan.
