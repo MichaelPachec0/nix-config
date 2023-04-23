5566e2d33b6c1009185d4dd7df993d5aec9d0a99
Michael Pacheco <git@michaelpacheco.org>
2023-04-23 01:02:49 -0700

    FIX: misspellings.

--- features/hm/common/programs.nix
+++ features/hm/common/programs.nix
@@ -12,21 +12,12 @@
         };
       };
       # cmd fuzzy finder, for now default to using fzf
-<<<<<<< HEAD
-       skim = {
-              enable = true;
-              enableZshIntegration = true;
-            };
-      # downloader
-      aria.enable = true;
-=======
       skim = {
         enable = true;
         enableZshIntegration = true;
       };
       # downloader
       aria2.enable = true;
->>>>>>> 72386ef (FIX: misspellings.)
       # cheat sheet commands
       navi = {
         enable = true;
@@ -37,11 +28,7 @@
       # smarter cd ( with caveats)
       zoxide.enable = true;
       # terminal git ui (uses ncurses iirc)
-<<<<<<< HEAD
-      gutui.enable = true;
-=======
       gitui.enable = true;
->>>>>>> 72386ef (FIX: misspellings.)
       # modern ls alterantive
       exa = {
         enable = true;
@@ -62,14 +49,6 @@
 
     } // lib.attrsets.optionalAttrs (config.graphical.enable) {
       # terminal that uses gpu for fast rendering
-<<<<<<< HEAD
-      kitty = {
-        enable = true;
-        package = pkgs.unstable.kitty;
-        theme = "Gruvbox Material Dark Hard";
-      };
-=======
->>>>>>> 72386ef (FIX: misspellings.)
       foot = { enable = true; };
       # launcher
       rofi = {
@@ -78,11 +57,7 @@
       };
       firefox = {
         enable = true;
-<<<<<<< HEAD
-        package = pkgs.firefox-devedition-bit;
-=======
         package = pkgs.firefox-devedition-bin;
->>>>>>> 72386ef (FIX: misspellings.)
       };
     };
   };
