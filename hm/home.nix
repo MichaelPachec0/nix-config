783c42e5ab0b3ae620a5b5298ad4aa83490b5b95
Michael Pacheco <git@michaelpacheco.org>
2023-04-25 22:35:44 -0700

    FEAT: enable service switching for home manager when activating the generation.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- hm/home.nix
+++ hm/home.nix
@@ -79,6 +79,8 @@ in {
     ];
 
   };
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
