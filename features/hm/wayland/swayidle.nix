dfbf516fc71f70d7ca08e32df86822a93a8129d9
Michael Pacheco <git@michaelpacheco.org>
2023-04-27 18:37:43 -0700

    FEAT: swaylock: make sure we are pulling from unstable.
    * nixpkgs-wayland is still pulling from the original repo, which is abandoned.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/hm/wayland/swayidle.nix
+++ features/hm/wayland/swayidle.nix
@@ -3,7 +3,10 @@ let nw = inputs.nixpkgs-wayland.packages.${pkgs.system};
 in {
   config = {
     services.swayidle = let
-      lockScreen = "${nw.swaylock-effects}/bin/swaylock -fF";
+      swaylockSettings =
+        " --screenshots  --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2 -fF";
+      lockScreen =
+        "${config.programs.swaylock.package}/bin/swaylock ${swaylockSettings}";
       hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
     in {
       enable = true;
