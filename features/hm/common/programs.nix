0b8e11d075ab6ba9fee040e34b2276ae13ba2637
Michael Pacheco <git@michaelpacheco.org>
2023-07-26 17:31:42 -0700

    Rework programs attrset.
    * Might redo this to flatten the actual attrsets instead of having them nested.
    * added a bit more git config. Not really commit worthy.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/hm/common/programs.nix
+++ features/hm/common/programs.nix
@@ -1,54 +1,57 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   config = {
-    programs = (lib.attrsets.recursiveUpdate (lib.attrsets.recursiveUpdate
+    programs = lib.attrsets.recursiveUpdate (lib.attrsets.recursiveUpdate
       (lib.attrsets.recursiveUpdate {
-        # cat alternative
-        bat.enable = true;
-        # tldr alternative
-        tealdeer = {
-          enable = true;
-          settings = {
-            display.use_pager = true;
-            updates.auto_update = true;
+          # cat alternative
+          bat.enable = true;
+          # tldr alternative
+          tealdeer = {
+            enable = true;
+            settings = {
+              display.use_pager = true;
+              updates.auto_update = true;
+            };
           };
-        };
-        # cmd fuzzy finder, for now default to using fzf
-        skim = {
-          enable = true;
-          enableZshIntegration = true;
-        };
-        # downloader
-        aria2.enable = true;
-        # cheat sheet commands
-        navi = {
-          enable = true;
-          enableZshIntegration = true;
-        };
-        # terminal document converter
-        pandoc.enable = true;
-        # smarter cd ( with caveats)
-        zoxide.enable = true;
-        # terminal git ui (uses ncurses iirc)
-        gitui.enable = true;
-        # modern ls alterantive
-        exa = {
-          enable = true;
-          icons = true;
-          git = true;
-          enableAliases = true;
-          extraOptions = [
-            "--group-directories-first"
-            "--header"
-            "--group"
-            "--time-style=long-iso"
-            "--extended"
-          ];
-        };
-
-      } (lib.attrsets.optionalAttrs (config.audio.enable) {
-        ncmpcpp.enable = true;
-
-      })) (lib.attrsets.optionalAttrs (config.graphical.enable) {
+          # cmd fuzzy finder, for now default to using fzf
+          skim = {
+            enable = true;
+            enableZshIntegration = true;
+          };
+          # downloader
+          aria2.enable = true;
+          # cheat sheet commands
+          navi = {
+            enable = true;
+            enableZshIntegration = true;
+          };
+          # terminal document converter
+          pandoc.enable = true;
+          # smarter cd ( with caveats)
+          zoxide.enable = true;
+          # terminal git ui (uses ncurses iirc)
+          gitui.enable = true;
+          # modern ls alterantive
+          exa = {
+            enable = true;
+            icons = true;
+            git = true;
+            enableAliases = true;
+            extraOptions = [
+              "--group-directories-first"
+              "--header"
+              "--group"
+              "--time-style=long-iso"
+              "--extended"
+            ];
+          };
+        } (lib.attrsets.optionalAttrs config.audio.enable {
+          ncmpcpp.enable = true;
+        })) (lib.attrsets.optionalAttrs config.graphical.enable {
         # terminal that uses gpu for fast rendering
         foot = { enable = true; };
         # launcher
@@ -95,37 +98,43 @@
           enable = true;
           package = pkgs.firefox-devedition-bin;
         };
-
-      })) (lib.attrsets.optionalAttrs (config.devMachine.enable) {
-        direnv = {
-          enable = true;
-          nix-direnv.enable = true;
+      })) (lib.attrsets.optionalAttrs config.devMachine.enable {
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+      gh = {
+        enable = true;
+        extensions = [pkgs.gh-eco pkgs.gh-dash];
+        settings = {
+          editor = "vim";
+          git_protocol = "ssh";
         };
-        gh = {
-          enable = true;
-          extensions = [ pkgs.gh-eco pkgs.gh-dash ];
-          settings = {
-            editor = "vim";
-            git_protocol = "ssh";
-
-          };
+      };
+      vscode = {
+        enable = true;
+        enableExtensionUpdateCheck = false;
+        enableUpdateCheck = false;
+        mutableExtensionsDir = true;
+        userSettings = {
+          "nix.serverPath" = "nil";
         };
-        git = {
+      git = {
+        enable = true;
+        package = pkgs.gitFull;
+        userName = "Michael Pacheco";
+        userEmail = "git@michaelpacheco.org";
+        ignores = ["*~" "*.swap" ".vscode" ".idea"];
+        diff-so-fancy = {
           enable = true;
-          package = pkgs.gitFull;
-          userName = "Michael Pacheco";
-          userEmail = "git@michaelpacheco.org";
-          ignores = [ "*~" "*.swap" ".vscode" ".idea" ];
-          diff-so-fancy = {
-            enable = true;
-            markEmptyLines = true;
-            useUnicodeRuler = true;
-          };
-          signing = {
-            signByDefault = true;
-            key = "2A1E939CF48AC3CC";
-          };
+          markEmptyLines = true;
+          useUnicodeRuler = true;
         };
-      }));
+        signing = {
+          signByDefault = true;
+          key = "2A1E939CF48AC3CC";
+        };
+      };
+    });
   };
 }
