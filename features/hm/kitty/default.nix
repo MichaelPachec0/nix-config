855066bdf9c1ac95e4b0fa875bedbf59502b05e2
Michael Pacheco <git@michaelpacheco.org>
2023-08-04 18:42:35 -0700

    FEAT: HM: rework 🐈 options.
    * Fixes nerdfont access.
    * As well some formatting fixes.
    * Extra comments, because comments are the best.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/hm/kitty/default.nix
+++ features/hm/kitty/default.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   imports = [ ];
   options = { };
+{pkgs, ...}: {
+  imports = [];
+  options = {};
   config = {
     nixpkgs = {
       overlays =
@@ -10,29 +13,35 @@
       kitty = {
         enable = true;
         package = pkgs.unstable.kitty;
+        # NOTE: might contribute extra options to this, a module for theme that can specify the package as well
         theme = "Gruvbox Material Dark Hard";
         font = {
-          name = "JetBrains Mono Regular Nerd Font Complete";
-          package = pkgs.jetbrains-mono;
+          # NOTE: This should be install globally as part of fontConfig
+          # prefer this to FiraCode, the r's are more reable with the current size
+          name = "JetBrainsMono NF Regular";
           size = 9;
         };
         settings = {
+          # NOTE: want a *huge* buffer, dont really care about the memory usuage,
+          #   should have enough.
           scrollback_lines = 100000;
           enable_audio_bell = true;
-          bold_font = "JetBrains Mono Bold Nerd Font Complete";
-          italic_font = "JetBrains Mono SemiBold Italic Nerd Font Complete";
-          bold_italic_font = "JetBrains Mono Bold Italic Nerd Font Complete";
+          bold_font = "JetBrainsMono NF Bold";
+          italic_font = "JetBrainsMono NF Italic";
+          bold_italic_font = "JetBrainsMono NF Bold Italic";
           strip_trailing_spaces = "smart";
           enabled_layouts = "Splits";
           window_border_width = "4.0pt";
           inactive_border_color = "#5c5c5c";
           draw_minimal_borders = "yes";
-          #window_margin_width = "1";
-          # TODO: setup later
+          # WARN: Did not like change, might modify later.
+          # window_margin_width = "1";
+          # TODO: (med prio) setup later
           # tab_bar_style = "custom";
         };
-        # need to set more keybindings
+        # TODO: (low prio) need to set more keybindings
         # ref: https://sw.kovidgoyal.net/kitty/layouts/#the-splits-layout
+        # NOTE: for todo: the most important keybindings are already setup.
         keybindings = {
           # ctrl+shift+\
           "kitty_mod+0x5c" = "launch --location=vsplit";
