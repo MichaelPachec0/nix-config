6c534ea70fc37f5196c977025dea7f558f96775a
Michael Pacheco <git@michaelpacheco.org>
2023-09-02 17:05:20 -0700

    FEAT: hm: add package change reporting.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- features/hm/common/default.nix
+++ features/hm/common/default.nix
@@ -5,5 +5,6 @@
       lib.mkEnableOption "Enable graphical programs common in all desktops";
     audio.enable =
       lib.mkEnableOption "Enable audio programs common in all desktops";
+    report-changes.enable = lib.mkEnableOption "Create a report after sucessful home-manager generation";
   };
 }
--- features/hm/common/services.nix
+++ features/hm/common/services.nix
@@ -1,14 +1,23 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options = {};
-  config = {
-    services =
-      lib.recursiveUpdate {}
-      (lib.attrsets.optionalAttrs (config.audio.enable) {
-        playerctld.enable = true;
-      });
-  };
+  config = let
+    inherit (config.lib) dag;
+  in
+    {
+      services =
+        lib.recursiveUpdate {}
+        (lib.attrsets.optionalAttrs config.audio.enable {
+          playerctld.enable = true;
+        });
+    }
+    // lib.mkIf config.report-changes.enable {
+      home.activation.report-changes = dag.entryAnywhere ''
+        ${lib.getExe pkgs.nvd} diff $oldGenPath $newGenPath
+      '';
+    };
 }
