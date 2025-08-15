8470786fae9a75f6c11b5a894476ae4bf7029c3d
Michael Pacheco <git@michaelpacheco.org>
2025-08-14 19:35:47 -0700

    nixos/common: keys.nix deploy, neovim input, dev/vm pkgs, gen diff
    deploy.nix: authorized keys from keys.all (no-pty), allowed-users, deploy-rs
    sudo rules. default.nix: machine.vm/report-changes options, neovim flake input
    as editor, nvd generation-diff activation, logrotate/i18n/udev, package renames
    (thefuck->pay-respects, wakatime->wakatime-cli, usbmuxd2->usbmuxd).

--- features/nixos/common/default.nix
+++ features/nixos/common/default.nix
@@ -1,16 +1,135 @@
-{ config, pkgs, lib, inputs, ... }: {
-  imports = [ ../../../overlays/modules/usbmuxd ];
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: let
+  report-changes = config.report-changes.enable;
+in {
+  # imports = [../../../overlays/modules/usbmuxd];
   options = {
     audio.enable = lib.mkEnableOption "Installs common audio apps.";
     devMachine.enable = lib.mkEnableOption "Install common developer apps.";
+    report-changes.enable = lib.mkEnableOption "Create a report after successful nixOS generation creation";
+    machine.vm.enable = lib.mkEnableOption "Not a real machine";
   };
-  config = {
-    nixpkgs.overlays = [ inputs.nix-your-shell.overlays.default ];
+  config = let
+  in {
     programs.neovim = {
+      package = inputs.neovim.packages.${pkgs.stdenv.hostPlatform.system}.default;
       enable = true;
       defaultEditor = true;
+      vimAlias = true;
+      viAlias = true;
     };
+    documentation = {
+      enable = true;
+      dev.enable = true;
+    };
+
+    services.logrotate = {
+      enable = true;
+    };
+
     programs.zsh.enable = true;
+    nixpkgs.overlays = [
+      # 2025-1-10 removed these as these are old
+      (final: prev: {
+        # libplist = prev.libplist.overrideAttrs (old: rec {
+        #   version = "2.6.1";
+        #   src = prev.fetchFromGitHub {
+        #     owner = old.src.owner;
+        #     repo = old.pname;
+        #     rev = "e8791e2d8b1d1672439b78d31271a8cf74d6a16d";
+        #     hash = "sha256-sKLFfv+B5UuYjMxG8a6GbP6BvohkhkqjS5+RBncHvxI=";
+        #   };
+        #   preAutoreconf = ''
+        #     export RELEASE_VERSION=${version}
+        #   '';
+        # });
+        # libimobiledevice-glue = (prev.libimobiledevice-glue.override {inherit (final) libplist;}).overrideAttrs (old: {
+        #   # NOTE: this differiates from nixpkgs version.
+        #   version = "0.0.0-master";
+        #   src = prev.fetchFromGitHub {
+        #     owner = old.src.owner;
+        #     repo = old.pname;
+        #     rev = "14c2e4b64b2bd6189d06d858bf4898d3a9f5a6e4";
+        #     hash = "sha256-QNwyvPAY46a/jtpyPhKyuBc9ATWtgwmRDgjtlpZ3BTo=";
+        #   };
+        # });
+        # libusbmuxd = prev.libusbmuxd.override {inherit (final) libplist;};
+        # libimobiledevice = (prev.libimobiledevice.override {inherit (final) libplist libimobiledevice-glue libusbmuxd;}).overrideAttrs (old: {
+        #   # NOTE: this differiates from nixpkgs version.
+        #   version = "0.0.0-master";
+        #   src = prev.fetchFromGitHub {
+        #     owner = old.src.owner;
+        #     repo = old.pname;
+        #     rev = "9ccc52222c287b35e41625cc282fb882544676c6";
+        #     hash = "sha256-pNvtDGUlifp10V59Kah4q87TvLrcptrCJURHo+Y+hs4=";
+        #   };
+        #   # NOTE: patches to compile with recent clang are not needed anymore.
+        #   patches = [];
+        # });
+        # libtatsu = prev.callPackage ({
+        #   lib,
+        #   stdenv,
+        #   fetchFromGitHub,
+        #   autoreconfHook,
+        #   pkg-config,
+        #   curl,
+        #   libplist,
+        # }:
+        #   stdenv.mkDerivation rec {
+        #     pname = "libtatsu";
+        #     version = "1.0.0";
+        #
+        #     src = fetchFromGitHub {
+        #       owner = "libimobiledevice";
+        #       repo = pname;
+        #       rev = "6fd8a51bbdb4411915663f9686d4a2045f61997c";
+        #       hash = "sha256-6mg/sVh4tHOLatEhwc2pvqpxrObZPnHdLBORWMLc7bA=";
+        #     };
+        #     outputs = ["out" "dev"];
+        #     nativeBuildInputs = [
+        #       autoreconfHook
+        #       pkg-config
+        #     ];
+        #     buildInputs = [
+        #       curl
+        #       libplist
+        #     ];
+        #     preAutoreconf = ''
+        #       export RELEASE_VERSION=${version}
+        #     '';
+        #     meta = {
+        #       description = "Library handling the communication with Apple's Tatsu Signing Server (TSS)";
+        #       homepage = "https://github.com/libimobiledevice/libtatsu";
+        #       license = lib.licenses.lgpl21Plus;
+        #       # maintainers = with lib.maintainers; [  ];
+        #     };
+        #   }) {};
+        # idevicerestore =
+        #   prev
+        #   .idevicerestore
+        #   .overrideAttrs (old: {
+        #     version = "1.0.0+date=2024-06-04";
+        #     src = prev.fetchFromGitHub {
+        #       owner = old.src.owner;
+        #       repo = old.pname;
+        #       rev = "df06f4d859f7bb0896d1b15ade5b9d2b58626a0e";
+        #       hash = "sha256-CzUOrumygTa3lPTD9vZutVEyloCVVnJ0BA0vH7KOvd4=";
+        #     };
+        #     buildInputs = old.buildInputs ++ [final.libtatsu];
+        #     patches = (old.patches or []) ++ [../../../helpers/idevicerestore.patch];
+        #   });
+        # buildInputs = [t final.libimobiledevice final.libusbmuxd final.libirecovery];
+        # nativeBuildInputs = old.nativeBuildInputs ++ [libplist];
+      })
+    ];
+
+    # NOTE: faster dbus implementation than default daemon
+    # services.dbus.implementation = "broker";
 
     # Enable tor
     services.tor = {
@@ -24,33 +143,39 @@
         };
         dns.enable = true;
       };
-      settings = { ClientUseIPv4 = true; };
+      settings = {ClientUseIPv4 = true;};
     };
+
     services.privoxy = {
       enable = true;
       enableTor = true;
     };
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
     environment.systemPackages = with pkgs;
       [
         nix-your-shell
+        # brings gix a faster git client
+        gitoxide
+        gitFull
         curl
         wget
-        # graphical/terminal browser
-        links2
         # find alternative
         fd
         # better grep
         ripgrep
         # terminal file manager
-        joshuto
+        # WARN: disabled for kore since rustc has not been updated for 1.74
+        # joshuto
         # images in the terminal
-        viu
-        neofetch
-        thefuck
-        powertop-git
-        pciutils
-        usbutils
-        # extract 
+        # viu
+        # neofetch
+        # thefuck
+        pay-respects
+        # powertop
+        # extract
         unzip
         zip
         p7zip
@@ -69,27 +194,110 @@
         nvd
         # process mon
         btop
-        unstable.home-manager
+        home-manager
 
-        #iOS
-        libimobiledevice
-        ifuse
-        idevicerestore
+        # iOS
+        # libimobiledevice
+        # ifuse
+        # idevicerestore
+
+        # Hex stuff
+        ## hex editor
+        ### TODO: Decide on one
+        # TODO: remove for now, decide to fix later
+        # (hexcurse.overrideAttrs (old: {
+        #   src = pkgs.fetchFromGitHub {
+        #     owner = "prso";
+        #     repo = "hexcurse-ng";
+        #     rev = "1c15a63d6b0e7c03bba96d83a72bf0c64f6be296";
+        #     hash = "sha256-PIVApm1ZkW9ApptNasQGGi2o6OifI8DNfnca3Ew9Eks=";
+        #   };
+        #   # remove obsolete patches
+        #   patches = [];
+        # }))
+        ### hex viewer with diff
+        # dhex
+        ### don't know about this one. in go with hex on top of ascii
+        hecate
+        ## hex viewer
+        ### rust hex viewer
+        hexyl
+        ### also rust but with vim keybinds
+        # xxv
+        ## cool hex things
+        ### hex visualizer
+        pixd
+        shellcheck
+        # hacks around color removal when piping
+        expect
+        # space age sed, fancy sed: https://github.com/ms-jpq/sad
+        sad
+
+        jq
+        # man-pages
+        # man-pages-posix
       ]
-      ++ lib.optionals (config.audio.enable) [ pavucontrol ncpamixer playerctl ]
-      ++ lib.optionals (config.devMachine.enable) [
+      ++ lib.optionals config.audio.enable [pavucontrol ncpamixer playerctl]
+      ++ lib.optionals (!config.machine.vm.enable) [
+        # graphical/terminal browser
+        # links2
+        pciutils
+        usbutils
+      ]
+      ++ lib.optionals config.devMachine.enable [
+        powertop-git
+        nixpkgs-review
+        crate2nix
         nix-prefetch
         nix-prefetch-git
         nix-prefetch-github
         nix-index
         nixfmt
+        # WARN: switch to flake based dev, do not need a global rust-analyzer or clippy
+        # rust-analyzer
+        # clippy
         # csv files in terminal
         tidy-viewer
-      ] ++ lib.optionals (config.services.hardware.bolt.enable) [ bolt ];
+        android-tools
+        # 2025-11-05: wakatime to wakatime-cli
+        wakatime-cli
+      ]
+      ++ lib.optionals config.services.hardware.bolt.enable [bolt];
+    # TODO: move to using nixos module. Might be a bit difficult since the nixos stable does not have this merged, but unstable does.
     services.usbmuxd = {
       enable = true;
-      package = pkgs.unstable.usbmuxd2;
+      package = pkgs.usbmuxd;
+    };
+    services.udev.packages = with pkgs;
+      lib.optionals config.devMachine.enable [
+        # 2025-11-11: android-udev-rules is now standard, the package is now EOL
+        # android-udev-rules
+        polar
+      ];
+
+    # systemd.= ''
+    #   DefaultTimeoutStopSec=10s
+    # '';
+    # NOTE: newer unstable does this.
+    systemd.settings.Manager = {
+      DefaultTimeoutStopSec = "10s";
+    };
+    # NOTE: gives a really nice diff between generations
+    # src: https://github.com/luishfonseca/dotfiles/blob/32c10e775d9ec7cc55e44592a060c1c9aadf113e/modules/upgrade-diff.nix
+    system.activationScripts.diff = {
+      supportsDryActivation = true;
+      # ${lib.getExe pkgs.nvd} --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+      text = ''
+        if [[ -e /run/current-system ]]; then
+          echo "#############################        diff to current-system        ##############################"
+          echo "#                                                                                               #"
+          ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff $(${pkgs.coreutils}/bin/readlink "/run/current-system") "$systemConfig" | tee /etc/gradientos-changelog
+          echo "#                                                                                               #"
+          echo "#############################      end diff to current-system      ##############################"
+        fi
+      '';
     };
   };
+  # // (lib.optionalAttrs report-changes {
+  # });
 }
-
--- features/nixos/common/deploy.nix
+++ features/nixos/common/deploy.nix
@@ -1,42 +1,89 @@
-{ pkgs, ... }: {
-  users.groups.deploy = { };
+{pkgs, ...}: let
+  keys = import ../../../keys.nix;
+in {
+  # make sure that this user is allowed to manipulate the store.
+  nix.settings.allowed-users = ["deploy"];
+  users.groups.deploy = {};
   users.users.deploy = {
     isSystemUser = true;
     group = "deploy";
     shell = pkgs.bash;
 
-    openssh.authorizedKeys.keys = [
-      "no-pty sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICFSUN6IGskLmeq7ip+oTbYuE+WRLcbYGGGOAyH/ECWaAAAABHNzaDo= michael@nyx"
-      "no-pty sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAKEnIsOFEp1Lx9XwZRVN+iRKyCKRiy4U9kw1JWH1UAYAAAABHNzaDo= michael@nyx"
-      "no-pty "
-      "no-pty "
-    ];
+    # michael's YubiKey sk-keys + thanatos, restricted with no-pty (see ../../../keys.nix).
+    openssh.authorizedKeys.keys = map (k: "no-pty ${k}") keys.all;
   };
 
-  security.sudo.extraRules = [{
-    groups = [ "deploy" ];
-    commands = [
-      {
-        command = "/nix/store/*/bin/switch-to-configuration";
-        options = [ "NOPASSWD" ];
-      }
-      {
-        command = "/run/current-system/sw/bin/nix-store";
-        options = [ "NOPASSWD" ];
-      }
-      {
-        command = "/run/current-system/sw/bin/nix-env";
-        options = [ "NOPASSWD" ];
-      }
-      {
-        command = ''
-          /bin/sh -c "readlink -e /nix/var/nix/profiles/system || readlink -e /run/current-system"'';
-        options = [ "NOPASSWD" ];
-      }
-      {
-        command = "/run/current-system/sw/bin/nix-collect-garbage";
-        options = [ "NOPASSWD" ];
-      }
-    ];
-  }];
+  security.sudo.extraRules = [
+    {
+      groups = ["deploy"];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/nix-store";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-env";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = ''
+            /bin/sh -c "readlink -e /nix/var/nix/profiles/system || readlink -e /run/current-system"'';
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-collect-garbage";
+          options = ["NOPASSWD"];
+        }
+        # NOTE: pre 23.11 this was the way systems were reconfigured (auc), newer systems uses systemd-run and env
+        {
+          command = "/nix/store/*/bin/switch-to-configuration";
+          options = ["NOPASSWD"];
+        }
+        # NOTE: this is needed for 23.11
+        # https://nixos.org/manual/nixos/stable/release-notes#sec-release-23.11-nixos-breaking-changes
+        {
+          command = "/run/current-system/sw/bin/systemd-run";
+          options = ["NOPASSWD"];
+        }
+        # NOTE: this is also needed for 23.11
+        # $ ssh -o ControlMaster=auto -o ControlPath=/tmp/nixos-rebuild.hyg5TQ/ssh-%n -o ControlPersist=60 -t deploy@kore sudo --preserve-env=NIXOS_INSTALL_BOOTLOADER -- env -i LOCALE_ARCHIVE=/run/current-system/sw/lib/locale/locale-archive NIXOS_INSTALL_BOOTLOADER= /nix/store/wb3gf1755j2f0vc7ag46hjfvmbvcldfw-nixos-system-kore-23.11.20240222.3cb4ae6/bin/switch-to-configuration switch
+        # PTY allocation request failed
+        #
+        # ... trancated for brevity ...
+        #
+        # sudo: a password is required
+        {
+          command = "/run/current-system/sw/bin/env";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/ln";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-copy-closure";
+          options = ["NOPASSWD"];
+        }
+
+        # {
+        #   command = "/run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pty --quiet --same-dir --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait true";
+        #   options = [ "NOPASSWD" ];
+        # }
+        # {
+        #   command = "/run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pty --quiet --same-dir --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait /nix/store/*/bin/switch-to-configuration *";
+        #   options = [ "NOPASSWD" ];
+        # }
+
+        # for deploy-rs
+        {
+          command = "/nix/store/*/activate-rs";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/rm /tmp/deploy-rs-canary-*";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
 }
