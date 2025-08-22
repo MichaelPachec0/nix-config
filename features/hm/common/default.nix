1206ad841d9b5e89bd8ff425c683e9b508ebd5eb
Michael Pacheco <git@michaelpacheco.org>
2025-08-21 22:02:59 -0700

    hm/common: add devMachine/gpu.strong/report-changes opts; nvd diff
    common/default.nix: nixfmt + three mkEnableOption flags (report-changes,
    devMachine, gpu.strong) and allowUnfree moved into this shared module.
    services.nix: report-changes activation running nvd diff on switch. (devMachine
    moved here out of neovim/default.nix.)

--- features/hm/common/default.nix
+++ features/hm/common/default.nix
@@ -1,9 +1,22 @@
-{ lib, ... }: {
-  imports = [ ./programs.nix ./services.nix ];
+{lib, ...}: {
+  imports = [./programs.nix ./services.nix];
   options = {
     graphical.enable =
       lib.mkEnableOption "Enable graphical programs common in all desktops";
     audio.enable =
       lib.mkEnableOption "Enable audio programs common in all desktops";
+    report-changes.enable = lib.mkEnableOption "Create a report after sucessful home-manager generation";
+    devMachine.enable =
+      lib.mkEnableOption
+      "Enables developer configuration. This includes certain packages as well as configuration.";
+    gpu.strong.enable =
+      lib.mkEnableOption
+      "device has a strong/discrete GPU; enables heavier visual effects (e.g. full-quality Hyprland blur)";
+  };
+  # Shared across all home-manager entrypoints (home.nix, home-test.nix, sysadmin.nix),
+  # all of which import this module.
+  config.nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = _: true;
   };
 }
