7825dc1ec68a6c9800340afcee65ae65089e6b1b
Michael Pacheco <git@michaelpacheco.org>
2025-09-09 20:13:14 -0700

    alex: add minimal Intel laptop host
    New minimal x86_64 host: systemd-boot + stateVersion 25.11; hardware-config with
    ext4-on-luks root/home, broadcom_sta (wl), intel microcode. Self-contained.

---
+++ nixos/alex/configuration.nix
@@ -0,0 +1,22 @@
+# TODO: CHANGE! should i do disk encryption
+# ask alex what items he wants installed in his config.
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  config = {
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.enable = false;
+    system.stateVersion = "25.11";
+  };
+}
