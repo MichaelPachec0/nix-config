5a002fb2bbe73882c4696f8ccbcf175bc865612c
Michael Pacheco <git@michaelpacheco.org>
2025-09-20 19:24:11 -0700

    nixos/server: add languageTool server module
    Adds the languageTool (bare/container) server options. Imported by kore and selene via the bare ../../features/nixos/server path (which resolves to default.nix), so it must ship alongside base.nix despite its in-file WIP marker.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

---
+++ features/nixos/server/default.nix
@@ -0,0 +1,31 @@
+# WARN: For now this is WIP, do not commit status, as this does not the ability to setup n-gram data. and ltrs at least will default
+#   to the public language api, that being said, it would be apt in the future to migrate to private language server.
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config;
+in
+  with lib; {
+    options = {
+      languageTool.bare.enable = mkEnableOption "Enable languageTool server";
+      languageTool.container.enable = mkEnableOption "Enable languageTool in a container";
+    };
+    config =
+      (mkIf cfg.languageTool.bare.enable {
+        services.languagetool = {
+          enable = true;
+          # NOTE: public in this case means that outside of localhost. This should be exposed in the overlay network.
+          public = true;
+          port = 6060;
+          settings = {
+            cacheSize = 10000;
+          };
+        };
+      }
+      // {
+        services.eternal-terminal.enable = true;
+      });
+  }
