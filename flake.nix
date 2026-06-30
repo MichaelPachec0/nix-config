6963d186fe4c77f37a5bc22c6ec186db0ec95119
Michael Pacheco <git@michaelpacheco.org>
2026-06-30 00:37:28 -0700

    chore(flake): update inputs; unpin swayfx scenefx follows
    Routine input bumps (nixpkgs, home-manager, neovim-nightly, nix-gaming, flake-playground, ...). swayfx no longer follows the top-level scenefx input (its flake.nix is ~a month stale), so it resolves its own pinned scenefx.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       url = "github:WillPower3309/swayfx?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
       # 2025-11-19: flake.nix for swayfx is month old
-      inputs.scenefx.follows = "scenefx";
+      # inputs.scenefx.follows = "scenefx";
     };
 
     kmonad-pkgs = {
